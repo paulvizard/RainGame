0611f556cf4889ece9c66376db189bdee3990644
Paul Vizard <paulv@protolink.com>
2020-01-30 20:26:02 -0600

    Init files

---
+++ src/com/golborne/rain/Game.java
@@ -0,0 +1,31 @@
+package com.golborne.rain;
+
+public class Game implements Runnable {
+    public static int width = 300;
+    public static int height = width / 16 * 9;
+    public static int scale = 3;
+
+    private Thread thread;
+    private boolean running = false;
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "Display");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        try {
+            running = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (running) {
+
+        }
+    }
+}
